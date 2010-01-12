8ec4714eb6d7e890647a83959eb58f00546a142a
sirprize <hoegl@backdoor.ch>
2010-01-12 20:30:04 +0100

    added version class

---
+++ lib/Xzend/Version.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Xzend - Zend Framework Extensions
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @package    Xzend
+ * @copyright  Copyright (c) 2009, Christian Hoegl, Switzerland (http://sitengine.org)
+ * @license    http://sitengine.org/license/new-bsd     New BSD License
+ */
+
+
+final class Xzend_Version
+{
+    
+    const VERSION = '0.1.0';
+
+
+    public static function compareVersion($version)
+    {
+        $version = strtolower($version);
+        $version = preg_replace('/(\d)pr(\d?)/', '$1a$2', $version);
+        return version_compare($version, strtolower(self::VERSION));
+    }
+}
